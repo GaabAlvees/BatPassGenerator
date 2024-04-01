91d9fbe4f2b5994bbce9de28132f8ac5897ae3f6
gaabalvees <gabriel.alvees@outlook.com.br>
2024-04-01 18:57:18 -0300

    Projeto BatPasswordGenerator

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Home from './src/screens/Home/Home';
+import { BatLogo } from './src/components/BatLogo/BatLogo';
+
+export default function App() {
+  return (
+      <Home/>  
+  );
+}
+
